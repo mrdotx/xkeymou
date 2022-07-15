8f724c4380ca1493193112ad85f39da788f51ff6
mrdotx <klassiker@gmx.de>
2022-07-15 14:05:25 +0200

    get direction and get_button added

--- util.c
+++ util.c
@@ -2,9 +2,10 @@
  * path:   /home/klassiker/.local/share/repos/xkeymou/util.c
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/xkeymou
- * date:   2022-07-05T10:30:16+0200
+ * date:   2022-07-15T14:02:45+0200
  */
 
+#include <stdio.h>
 #include <time.h>
 
 #include "util.h"
@@ -21,4 +22,34 @@ void sleep_ms(long ms) {
     };
 
     nanosleep(&request, &remaining);
+}
+
+const char *get_direction(int x, int y) {
+    static char string[11];
+
+    if (0 == x && 0 == y) sprintf(string, "STOPPED");
+
+    if (0 == x && 0 > y) sprintf(string, "UP");
+    if (0 == x && 0 < y) sprintf(string, "DOWN");
+    if (0 > x && 0 == y) sprintf(string, "LEFT");
+    if (0 < x && 0 == y) sprintf(string, "RIGHT");
+
+    if (0 > x && 0 > y) sprintf(string, "UP-LEFT");
+    if (0 < x && 0 > y) sprintf(string, "UP-RIGHT");
+    if (0 > x && 0 < y) sprintf(string, "DOWN-LEFT");
+    if (0 < x && 0 < y) sprintf(string, "DOWN-RIGHT");
+
+    return string;
+}
+
+const char *get_button(int button) {
+    static char string[7];
+
+    if (0 == button) sprintf(string, "DUMMY");
+
+    if (1 == button) sprintf(string, "LEFT");
+    if (2 == button) sprintf(string, "MIDDLE");
+    if (3 == button) sprintf(string, "RIGHT");
+
+    return string;
 }
--- util.h
+++ util.h
@@ -2,7 +2,7 @@
  * path:   /home/klassiker/.local/share/repos/xkeymou/util.h
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/xkeymou
- * date:   2022-07-05T09:32:01+0200
+ * date:   2022-07-15T14:02:17+0200
  */
 
 /* macros */
@@ -10,3 +10,5 @@
 
 int nanosleep(const struct timespec *req, struct timespec *rem);
 void sleep_ms(long ms);
+const char *get_direction(int x, int y);
+const char *get_button(int button);

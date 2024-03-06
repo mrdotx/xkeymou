2730750beac8d691592d576d649b33e73a7280de
mrdotx <klassiker@gmx.de>
2024-03-06 08:02:55 +0100

    call to undeclared functions fixed

--- xkeymou.h
+++ xkeymou.h
@@ -2,7 +2,7 @@
  * path:   /home/klassiker/.local/share/repos/xkeymou/xkeymou.h
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/xkeymou
- * date:   2022-07-16T11:53:09+0200
+ * date:   2024-03-06T08:02:38+0100
  */
 
 #include <X11/XKBlib.h>
@@ -45,6 +45,7 @@ void move_relative(float x, float y);
 void click(unsigned int button, int is_press);
 void click_full(unsigned int button);
 void scroll(float x, float y);
-void handle_key(KeyCode keycode, int is_press, int is_debug);
 void init_x();
-void close_x();
+void close_x(int exit_status);
+void *move_forever(void *val);
+void handle_key(KeyCode keycode, int is_press, int is_debug);
